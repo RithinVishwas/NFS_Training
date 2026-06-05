ab147e90285a8c7bbb4b2732fdc6cfd9aadccb55
Rithin Vishwas <132218935+RithinVishwas@users.noreply.github.com>
2026-06-05 12:33:14 +0530

    Add files via upload

---
+++ Day12/crudOperation/crudOperation/test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+class Test
+{
+    static void Main()
+    {
+        string cs = @"Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        using (SqlConnection conn = new SqlConnection(cs))
+        {
+            conn.Open();
+            Console.WriteLine("Connected!");
+            conn.Close();
+        }
+    }
+}
